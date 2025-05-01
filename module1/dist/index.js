ee8004953cab34202ef41bffb19a01dbe9881008
Shahidullah-Kaisar <shkaisar2002@gmail.com>
2025-05-01 10:46:40 +0600

    basic data type

---
+++ module1/dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+let course = "Next level Web development";
+console.log(course);
---
+++ module1/src/1.4_Basic_DataType.ts
@@ -0,0 +1,21 @@
+//Basic Data Type
+
+//string
+let firstName : string = "Sajib"
+//number
+let roll : number = 123
+//boolean
+let isAdmin : boolean = true
+//undefined
+let x : undefined = undefined
+//null
+let y : null = null
+
+//array
+let friends : string[] = ['sajib', 'kaisar']
+
+let aligibleRollList : number[] = [1, 3]
+
+// tuple
+let coordinates : [number, number] = [1,5]
+let ageName : [number, string, boolean] = [50, "sajib", true]
